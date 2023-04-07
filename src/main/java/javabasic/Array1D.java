54ae78ad654c9f4d0d88c37dd43a1dbc422f4791
flaviaadiniz <flaviaadiniz@gmail.com>
2023-04-07 19:53:28 -0300

    Added Array1D and BigIntNumbers exercises

---
+++ src/main/java/javabasic/Array1D.java
@@ -0,0 +1,35 @@
+/*
+Create an array, a, capable of holding n integers.
+Modify the code in the loop so that it saves each sequential value to its corresponding location in the array.
+For example, the first value must be stored in a[0], the second value must be stored in a[1], and so on.
+ */
+
+package javabasic;
+
+import java.util.Scanner;
+
+public class Array1D {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("How many numbers in the array? ");
+        int n = scan.nextInt();
+
+        int[] a = new int[n];
+
+        for (int i = 0; i < a.length; i++) {
+            System.out.print("Type the number: ");
+            a[i] = scan.nextInt();
+        }
+
+        scan.close();
+
+        for (int num : a) {
+            System.out.println(num);
+        }
+
+    }
+
+}
---
+++ src/main/java/javabasic/BigIntNumbers.java
@@ -0,0 +1,35 @@
+/*
+In this problem, you have to add and multiply huge numbers! These numbers are so big that you can't contain
+them in any ordinary data types like a long integer. Use the power of Java's BigInteger class and solve this
+problem.
+
+Input Format
+There will be two lines containing two numbers, a and b.
+
+Output Format
+Output two lines. The first line should contain a + b, and the second line should contain a * b.
+Don't print any leading zeros.
+ */
+
+package javabasic;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class BigIntNumbers {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        BigInteger BigA = scanner.nextBigInteger();
+        BigInteger BigB = scanner.nextBigInteger();
+
+        scanner.close();
+
+        System.out.println(BigA.add(BigB));
+        System.out.println(BigA.multiply(BigB));
+
+    }
+
+}
