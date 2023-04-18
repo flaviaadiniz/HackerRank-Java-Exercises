9e22b561d76753e61ba1ff260e09e7c55607c37b
flaviaadiniz <flaviaadiniz@gmail.com>
2023-04-18 18:21:24 -0300

    Added Currency Formatter exercise

---
+++ src/main/java/javabasic/CurrencyFormatter.java
@@ -0,0 +1,31 @@
+/*
+Given a double-precision number, , denoting an amount of money, use the NumberFormat class' getCurrencyInstance
+method to convert  into the US, Indian, Chinese, and French currency formats.
+ */
+
+package javabasic;
+
+import java.text.NumberFormat;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class CurrencyFormatter {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double payment = scanner.nextDouble();
+        scanner.close();
+
+        String us = NumberFormat.getCurrencyInstance(Locale.US).format(payment);
+        NumberFormat formatter = NumberFormat.getCurrencyInstance(new Locale("en", "IN"));
+        String india = formatter.format(payment);
+        String china = NumberFormat.getCurrencyInstance(Locale.CHINA).format(payment);
+        String france = NumberFormat.getCurrencyInstance(Locale.FRANCE).format(payment);
+
+        System.out.println("US: " + us);
+        System.out.println("India: " + india);
+        System.out.println("China: " + china);
+        System.out.println("France: " + france);
+    }
+
+}
