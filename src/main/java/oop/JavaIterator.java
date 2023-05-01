53bb6436a0b76be07b328705eeb4a65d7e4acdcb
flaviaadiniz <flaviaadiniz@gmail.com>
2023-05-01 15:45:07 -0300

    Added JavaIterator exercise

--- src/main/java/oop/JavaIterator.java
+++ src/main/java/oop/JavaIterator.java
@@ -1,3 +1,12 @@
+/*
+In this problem you need to complete a method func. The method takes an ArrayList as input. In that ArrayList
+there is one or more integer numbers, then there is a special string "###", after that there are one or more
+other strings.
+
+You have to modify the func method by editing at most 2 lines so that the code only prints the elements after
+the special string "###".
+ */
+
 package oop;
 
 import java.util.*;
