e0e4dcbcff0d4c43341b5d2089bfbcf179345dc3
flaviaadiniz <flaviaadiniz@gmail.com>
2023-05-03 22:47:00 -0300

    Added JavaSubArray exercise

--- src/main/java/datastructures/JavaSubArray.java
+++ src/main/java/datastructures/JavaSubArray.java
@@ -1,37 +1,37 @@
+/*
+Given an array of  integers, find and print its number of negative subarrays on a new line.
+ */
+
 package datastructures;
 
 import java.util.Scanner;
 
-
-public class SubArray {
+public class JavaSubArray {
 
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-
         int n = scanner.nextInt();
-
         int[] numArray = new int[n];
 
         for (int i = 0; i < n; i++) {
             numArray[i] = scanner.nextInt();
         }
 
-        int[] subArrays;
+        int count = 0;
 
         for (int i = 0; i < n; i++) {
-
+            int sum = 0;
             for (int j = i; j < n; j++) {
-
-                for (int k = i; k <= j; k++) {
-
+                sum += numArray[j];
+                if (sum < 0) {
+                    count++;
                 }
             }
         }
 
+        System.out.println(count);
 
-
-
+        scanner.close();
     }
-
 }
