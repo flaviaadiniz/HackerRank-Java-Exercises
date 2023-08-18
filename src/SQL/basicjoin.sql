dc8f316226a05013c4f951abdb990950e38238bc
flaviaadiniz <flaviaadiniz@gmail.com>
2023-08-17 23:57:25 -0300

    Added SQL Basic Join - exercise 2

--- src/SQL/basicjoin.sql
+++ src/SQL/basicjoin.sql
@@ -5,3 +5,14 @@ SELECT SUM(city.population)
 FROM city
 JOIN country ON city.countrycode = country.code
 WHERE continent = 'Asia'
+
+
+/* Given the CITY and COUNTRY tables, query the names of all cities where the CONTINENT is 'Africa'.
+Note: CITY.CountryCode and COUNTRY.Code are matching key columns.*/
+
+SELECT city.name
+FROM city
+JOIN country
+ON city.countrycode = country.code
+WHERE continent = "Africa"
+ORDER BY city.name
