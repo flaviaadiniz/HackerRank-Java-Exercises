d62c2decb1708e9b6cc754d425bee34db37cd262
flaviaadiniz <flaviaadiniz@gmail.com>
2023-09-06 00:05:10 -0300

    Added SQL Aggregation - Exercise 16

--- src/SQL/aggregation.sql
+++ src/SQL/aggregation.sql
@@ -102,13 +102,19 @@ WHERE lat_n = (SELECT MIN(lat_n)
 
 
 /* 15. Consider P1(a,b) and P2(c,d) to be two points on a 2D plane.
-
  a happens to equal the minimum value in Northern Latitude (LAT_N in STATION).
  b happens to equal the minimum value in Western Longitude (LONG_W in STATION).
  c happens to equal the maximum value in Northern Latitude (LAT_N in STATION).
  d happens to equal the maximum value in Western Longitude (LONG_W in STATION).
-Query the Manhattan Distance between points  and  and round it to a scale of  decimal places. */
+Query the Manhattan Distance between points  and  and round it to a scale of 4 decimal places. */
 
 SELECT ROUND((MAX(lat_n) - MIN(lat_n)) + 
        (MAX(long_w) - MIN(long_w)), 4)
+FROM station;
+
+
+/* 16. Consider P1(a,c) and P2(b,d) to be two points on a 2D plane where (a,b) are the respective minimum and maximum values of Northern Latitude (LAT_N) and (c,d) are the respective minimum and maximum values of Western Longitude (LONG_W) in STATION.
+Query the Euclidean Distance between points P1 and P2 and format your answer to display 4 decimal digits. */
+
+SELECT ROUND(SQRT(POWER(MAX(lat_n) - MIN(lat_n), 2) + POWER(MAX(long_w) - MIN(long_w), 2)), 4)
 FROM station;
